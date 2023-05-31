611a06a3883fdf282ec846019ced94d14c4b526e
Jocelyn Velarde <a01285780@itesm.mx>
2023-05-31 01:25:01 -0600

    added lda plot chart

--- BCI_codes/BCI_2.py
+++ BCI_codes/BCI_2.py
@@ -1,6 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
 import pylsl
 import serial
-#The good one!
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
 # Open serial port
 ser = serial.Serial('COM5', 9600)
 
@@ -12,49 +15,83 @@ if not streams:
 else:
     # Create a new inlet to read from the stream
     inlet = pylsl.stream_inlet(streams[0])
-    
+
     # Variables for storing information
-    info_matrix = []  # Matrix to store marker information
-    matrix_size = 17   # Number of samples to store in the matrix
-    left_count = 0    # Count of left markers
-    right_count = 0   # Count of right markers
+    infoMatrix = []  # Matrix to store marker information
+    matrixSize = 17  # Number of samples to store in the matrix
+    closeCount = 0  # Count of left markers
+    openCount = 0  # Count of right markers
+    closeSamples = []  # List to store "close" samples
+    openSamples = []  # List to store "open" samples
+    repeticiones_770 = 0  # Counter for consecutive repetitions of 770
+    repeticiones_769 = 0  # Counter for consecutive repetitions of 769
 
     while True:
         sample, timestamp = inlet.pull_sample()
 
         if sample[0] == 770:
-            left_count += 1
-            info_matrix.append(sample[0])
+            closeCount += 1
+            infoMatrix.append(sample[0])
+            repeticiones_770 += 1
+            repeticiones_769 = 0  # Reset the counter for 769 repetitions
+
+            if repeticiones_770 == 3:  # If 770 is repeated 3 times consecutively
+                sample[0] = 769  # Change 770 to 769
+                repeticiones_770 = 0  # Reset the counter for 770 repetitions
 
             # If the matrix is full, calculate the average and send the signal
-            if len(info_matrix) == matrix_size:
-                average = sum(info_matrix) / matrix_size
+            if len(infoMatrix) == matrixSize:
+                average = sum(infoMatrix) / matrixSize
 
                 if average == 770:
                     ser.write(b'O')
                 else:
-                    print("Right")
+                    print("Abrir")
 
-                info_matrix = []  # Clear the matrix after sending the signal
+                infoMatrix = []  # Clear the matrix after sending the signal
 
         elif sample[0] == 769:
-            right_count += 1
-            info_matrix.append(sample[0])
+            openCount += 1
+            infoMatrix.append(sample[0])
+            repeticiones_769 += 1
+            repeticiones_770 = 0  # Reset the counter for 770 repetitions
+
+            if repeticiones_769 == 3:  # If 769 is repeated 3 times consecutively
+                sample[0] = 770  # Change 769 to 770
+                repeticiones_769 = 0  # Reset the counter for 769 repetitions
 
             # If the matrix is full, calculate the average and send the signal
-            if len(info_matrix) == matrix_size:
-                average = sum(info_matrix) / matrix_size
+            if len(infoMatrix) == matrixSize:
+                average = sum(infoMatrix) / matrixSize
 
                 if average == 769:
                     ser.write(b'C')
                 else:
-                    print("Left")
+                    print("Cerrar")
 
-                info_matrix = []  # Clear the matrix after sending the signal
+                infoMatrix = []  # Clear the matrix after sending the signal
 
         else:
             print("Unknown marker")
 
         # Check if the matrix is full and print the counts
-        if len(info_matrix) == matrix_size:
-            print("Left: %d, Right: %d" % (left_count, right_count))
+        if len(infoMatrix) == matrixSize:
+            print("Close: %d, Open: %d" % (closeCount, openCount))
+
+        # Rest of the code...
+        # Rest of the code...
+        if len(closeSamples) >= 2 and len(openSamples) >= 2:
+            X = np.concatenate((closeSamples, openSamples))
+            y = np.concatenate((np.zeros(len(closeSamples)), np.ones(len(openSamples))))
+
+            lda = LinearDiscriminantAnalysis(n_components=2)
+            X_lda = lda.fit_transform(X, y)
+
+            plt.scatter(X_lda[:, 0], X_lda[:, 1], c=y, cmap='viridis')
+            plt.xlabel('LDA Component 1')
+            plt.ylabel('LDA Component 2')
+            plt.title('LDA Visualization')
+            plt.show()
+
+
+           
